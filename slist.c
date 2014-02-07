8cdec4fc3befcb69b16d7dbaede3f57577d42f57
Darcy Brás da Silva <dardevelin@cidadecool.com>
2014-02-07 17:25:19 +0000

    add slist_new_list implementation

--- slist.c
+++ slist.c
@@ -34,3 +34,27 @@ struct slist_list *slist_init(struct slist_list *list,
 	list->head = NULL;
 	return list;
 }/* slist_init */
+
+struct slist_list *slist_new_list(void *(*node_alloc)(size_t),
+				  void (*node_dalloc)(void *))
+{
+	node_alloc = (node_alloc ? node_alloc : SLIST_DEF_ALLOC);
+	node_dalloc = (node_dalloc ? node_dalloc : SLIST_DEF_DALLOC);
+
+	struct slist_list *list = NULL;
+
+	if ( NULL == (list = node_alloc( sizeof( struct slist_list))) ) {
+		//FIXME: add support for custom error logging and msg
+		fprintf(stderr,"%s[%d]:%s alloc failed\n", __FILE__,
+			__LINE__,__func__);
+
+		return NULL;
+	}
+
+	list->node_alloc = node_alloc;
+	list->node_dalloc = node_dalloc;
+	list->count = 0;
+	list->head = NULL;
+
+	return list;
+}/* slist_new_list */
